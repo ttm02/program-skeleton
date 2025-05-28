60f573392c629f1249a02af7e2683fe7e6c98457
Tim Jammer <tim.jammer@tu-darmstadt.de>
2025-05-28 14:06:08 +0200

    remove other use case for anonymity

--- use-cases/mpi-matching/clang_debug_wrapper.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# wrapper to debug clang invocations with more complicated build systems
-
-CLANG_WRAPPER=/work/home/tj75qeje/mpi-comp-match/build/clang_wrap_cxx
-
-# echo "$CLANG_WRAPPER $@" >> /home/tj75qeje/mpi-comp-match/build/clang_invoke_log
-
-DEBUG_CLANG_WRAPPER=true $CLANG_WRAPPER "$@" >> /home/tj75qeje/mpi-comp-match/build/clang_invoke_log
