14675ecb8bcc916b13f8c22e623f4a4b47bc76af
5ila5 <5ila5@users.noreply.github.com>
2025-09-21 17:48:05 +0200

    VtableManager Fix assertion error if llvm annotations are present

--- precomputation_compiler_lib/VtableManager.cpp
+++ precomputation_compiler_lib/VtableManager.cpp
@@ -52,7 +52,9 @@ VtableManager::get_vtable_from_ptr_user(llvm::User *vtable_value) {
     // we will just use the normal initializers
     // as everything of this will be called before main actually starts
     assert(current_level_of_definition->getName().equals("llvm.global_ctors") ||
-           current_level_of_definition->getName().equals("llvm.global_dtors"));
+           current_level_of_definition->getName().equals("llvm.global_dtors") ||
+           current_level_of_definition->getName().equals(
+               "llvm.global.annotations"));
     return nullptr;
   }
 
