521d40f72dbf2bdc358a8550388013b924506063
Tim Jammer <tim.jammer@tu-darmstadt.de>
2025-10-29 15:52:01 +0100

    fix tsan usecase

--- use-cases/thread-sanitizer/sanitizer_precompute_compiler_pass/sanitizer_precompute_pass.cpp
+++ use-cases/thread-sanitizer/sanitizer_precompute_compiler_pass/sanitizer_precompute_pass.cpp
@@ -207,14 +207,6 @@ struct SanitizerPrecomputePass : public PassInfoMixin<SanitizerPrecomputePass> {
 
     // do NOT call clean_precompute() as we want the tsan calls to stick around
 
-    // we don't need the management stuff, we directly replace our program with
-    // precomputed one
-    auto precompute_main = precalcuation->get_precompute_main();
-    auto it = precompute_main->begin()->begin();
-    ++it; // second instruction is call to precomputed main
-    auto *call = cast<CallBase>(it);
-    auto precomputed_main = call->getCalledFunction();
-
     // remove old main
     auto orig_linkeage = main_func->getLinkage();
     main_func->deleteBody(); // will set linkeage to weak, which we dont want
@@ -229,8 +221,8 @@ struct SanitizerPrecomputePass : public PassInfoMixin<SanitizerPrecomputePass> {
     for (auto &arg : main_func->args()) {
       args.push_back(&arg);
     }
-
-    builder.CreateCall(precomputed_main, args);
+    // call precompute main
+    builder.CreateCall(precalcuation->get_precompute_main(), args);
     builder.CreateRet(Constant::getNullValue(main_func->getReturnType()));
 
     remove_noinline_from_module(M);
