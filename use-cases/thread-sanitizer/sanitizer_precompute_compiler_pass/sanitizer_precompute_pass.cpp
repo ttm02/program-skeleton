71a8950458bfa2550f1684b1147d65be6d1a172b
Jan Braun <jan.braun@stud.tu-darmstadt.de>
2025-11-05 16:50:21 +0100

    outline pass steps in thread sanitizer optimizer

--- use-cases/thread-sanitizer/sanitizer_precompute_compiler_pass/sanitizer_precompute_pass.cpp
+++ use-cases/thread-sanitizer/sanitizer_precompute_compiler_pass/sanitizer_precompute_pass.cpp
@@ -44,20 +44,35 @@ static void remove_noinline_from_module(Module &M) {
   }
 }
 
-static void run_optimization_passes(Module &M, ModuleAnalysisManager &AM) {
-  errs() << "Run inliner Pass\n";
-  auto inliner = ModuleInlinerPass();
-  inliner.run(M, AM);
-
-  errs() << "Run Global DCE Pass\n";
-  auto dce = GlobalDCEPass();
-  dce.run(M, AM);
-
+static bool run_optimization_passes(
+    Module &M, ModuleAnalysisManager &AM,
+    std::string (*opt_pass_func)(Module &M, ModuleAnalysisManager &AM),
+    bool cleanup = true) {
+  std::string opt_msg_success = opt_pass_func(M, AM);
+  if (opt_msg_success.empty())
+    return false;
 #ifndef NDEBUG
-  auto has_error = verifyModule(M, &errs(), nullptr);
+  bool has_error = verifyModule(M, &errs(), nullptr);
   assert(!has_error);
 #endif
-  // M.dump();
+  errs() << opt_msg_success << "\n";
+
+  if (cleanup) {
+    errs() << "Run inliner Pass\n";
+    auto inliner = llvm::ModuleInlinerPass();
+    inliner.run(M, AM);
+
+    errs() << "Run Global DCE Pass\n";
+    auto dce = llvm::GlobalDCEPass();
+    dce.run(M, AM);
+
+    errs() << "\n";
+#ifndef NDEBUG
+    has_error = verifyModule(M, &errs(), nullptr);
+    assert(!has_error);
+#endif
+  }
+  return true;
 }
 
 static bool is_tsan_cleanup_block(BasicBlock *block) {
@@ -93,6 +108,150 @@ static bool is_compiled_with_flang(const Module &M) {
   return false;
 }
 
+static std::string run_tsan(Module &M, ModuleAnalysisManager &AM) {
+  auto tsan_func_entry = M.getFunction("__tsan_func_entry");
+  if (tsan_func_entry == nullptr || tsan_func_entry->users().empty()) {
+    errs() << "Run TSAN pass\n";
+    bool is_fortran_code = is_compiled_with_flang(M);
+    //  make sure TSAN pass runs
+    auto *FAM =
+        &AM.getResult<FunctionAnalysisManagerModuleProxy>(M).getManager();
+    auto tsan_pass = ThreadSanitizerPass();
+    for (auto it = M.begin(); it != M.end(); ++it) {
+      Function *f = &*it;
+      if (is_fortran_code) {
+        f->addFnAttr(Attribute::SanitizeThread);
+        // work around for fortran, as fortran does currently not support
+        // -fsanitize=thread flag
+      }
+      tsan_pass.run(*f, *FAM);
+    }
+    return "Successfully instrumented code with TSAN";
+  }
+  return "TSAN instrumentation already available";
+}
+
+static std::string run_precompute(Module &M, ModuleAnalysisManager &AM) {
+  allow_function_prefixes_to_be_called_in_precompute({"__tsan_"});
+
+  PrecomputeFunctions::create_instance(M);
+  analysis_results = new RequiredAnalysisResults(AM, M);
+
+  auto *main_func = M.getFunction("main");
+  assert(main_func);
+
+  std::vector<Value *> to_precompute;
+  std::vector<Instruction *> precompute_locations;
+
+  for (Function &f : M) {
+    if (not f.getName().starts_with("tsan.module_ctor")) {
+      // do not instrument tsan itself
+      for (BasicBlock &bb : f) {
+        for (Instruction &inst : bb) {
+          if (auto call = dyn_cast<CallBase>(&inst)) {
+            auto called_func = call->getCalledFunction();
+            if (!called_func)
+              continue;
+            auto func_name = called_func->getName();
+            // eiter tsan or omp function
+            // omp function necessary e.g. to keep synchronization
+            bool is_thread_func = is_thread_function(called_func);
+            if (func_name.starts_with("__tsan") || is_thread_func) {
+              if (func_name == "__tsan_func_exit") {
+                if (isa<ResumeInst>(call->getNextNonDebugInstruction())) {
+                  // TODO make this a compiler option!!
+                  //  in cleanup block, dont handle exceptions
+                  continue;
+                }
+                if (is_tsan_cleanup_block(call->getParent())) {
+                  // skip, the tsan cleanup part.
+                  // no need to precompute, as it will only handle fatal
+                  // exceptions
+                  continue;
+                }
+              }
+              if (is_func_from_std(call->getFunction())) {
+                // dont analyze internals of std, though tsan may instrument
+                // them
+                continue;
+              }
+
+              for (auto &it_arg : call->args())
+                to_precompute.push_back(it_arg);
+              precompute_locations.push_back(call);
+            }
+          }
+        }
+      }
+    }
+  }
+
+  errs() << "Statistics: locations: " << precompute_locations.size()
+         << " values: " << to_precompute.size() << "\n";
+
+  // no tsan found
+  if (precompute_locations.empty()) {
+    // no modification
+    return "";
+  }
+
+  auto precalcuation = std::make_shared<PrecomputeInsertion>(
+      M,
+      std::make_shared<PrecalculationAnalysis>(M, main_func, to_precompute,
+                                               precompute_locations),
+      false);
+
+  // do NOT call clean_precompute() as we want the tsan calls to stick around
+
+  // we don't need the management stuff, we directly replace our program with
+  // precomputed one
+  auto precompute_main = precalcuation->get_precompute_main();
+  auto it = precompute_main->begin()->begin();
+  ++it; // second instruction is call to precomputed main
+  auto *call = cast<CallBase>(it);
+  auto precomputed_main = call->getCalledFunction();
+
+  // remove old main
+  auto orig_linkeage = main_func->getLinkage();
+  main_func->deleteBody(); // will set linkeage to weak, which we dont want
+  main_func->setLinkage(orig_linkeage);
+
+  // create new function body
+  auto *bb = BasicBlock::Create(M.getContext(), "entry", main_func);
+  // add call to precompute
+  IRBuilder<> builder = IRBuilder<>(bb);
+  // forward args of main
+  std::vector<Value *> args;
+  for (auto &arg : main_func->args()) {
+    args.push_back(&arg);
+  }
+
+  builder.CreateCall(precomputed_main, args);
+  builder.CreateRet(Constant::getNullValue(main_func->getReturnType()));
+
+  remove_noinline_from_module(M);
+
+  // remove other non-precompute functions now
+  std::vector<Function *> to_delete;
+  for (auto it_f = M.begin(); it_f != M.end(); ++it_f) {
+    Function *f = &*it_f;
+    if (precalcuation->is_func_part_of_precompute_phase(f)) {
+      // the tsan calls are already part of precompute, no need to instrumente
+      // them again
+      f->removeFnAttr(Attribute::SanitizeThread);
+    } else if ((not f->isDeclaration()) && f != main_func &&
+               (not f->getName().starts_with("__tsan")) &&
+               (not is_func_from_std(f))) {
+      // not used: remove
+      if (f->hasExternalLinkage())
+        f->setLinkage(GlobalValue::InternalLinkage);
+      // this will prompt GlobalDCE to remove
+    }
+  }
+
+  return "Successfully computed the precomputation";
+}
+
 namespace {
 struct SanitizerPrecomputePass : public PassInfoMixin<SanitizerPrecomputePass> {
 
@@ -110,166 +269,15 @@ struct SanitizerPrecomputePass : public PassInfoMixin<SanitizerPrecomputePass> {
 
   // Pass starts here
   PreservedAnalyses run(Module &M, ModuleAnalysisManager &AM) {
-    Debug(errs() << "\n");
-
-    if (M.getFunction("__tsan_func_entry") == nullptr ||
-        (M.getFunction("__tsan_func_entry")->users().empty())) {
-
-      Debug(errs() << "Run TSAN pass\n\n");
-      bool is_fortran_code = is_compiled_with_flang(M);
-      //  make sure TSAN pass runs
-      auto *FAM =
-          &AM.getResult<FunctionAnalysisManagerModuleProxy>(M).getManager();
-      auto tsan_pass = ThreadSanitizerPass();
-      for (auto it = M.begin(); it != M.end(); ++it) {
-        Function *f = &*it;
-        if (is_fortran_code) {
-          f->addFnAttr(Attribute::SanitizeThread);
-          // work around for fortran, as fortran does currently not support
-          // -fsanitize=thread flag
-        }
-        tsan_pass.run(*f, *FAM);
-      }
-    }
-
-#ifdef VERBOSE_DEBUG_PRINTING
-    Debug(errs() << "Before Modification:\n"; M.dump();
-          errs() << "END MODULE\n";);
-#endif
-    auto has_error2 = verifyModule(M, &errs(), nullptr);
-    assert(!has_error2);
-
-    allow_function_prefixes_to_be_called_in_precompute({"__tsan_"});
-
-    PrecomputeFunctions::create_instance(M);
-    analysis_results = new RequiredAnalysisResults(AM, M);
-
-    auto num_undef = get_num_undefs(M);
-
-    auto *main_func = M.getFunction("main");
-    assert(main_func);
-
-    std::vector<Value *> to_precompute;
-    std::vector<Instruction *> precompute_locations;
-    for (auto it_f = M.begin(); it_f != M.end(); ++it_f) {
-      Function *f = &*it_f;
-      if (not f->getName().starts_with("tsan.module_ctor")) {
-        // do not instrument tsan itself
-        for (auto it_bb = f->begin(); it_bb != f->end(); ++it_bb) {
-          BasicBlock *bb = &*it_bb;
-          for (auto it_i = bb->begin(); it_i != bb->end(); ++it_i) {
-            Instruction *inst = &*it_i;
-            if (auto *call = dyn_cast<CallBase>(inst)) {
-              if (call->getCalledFunction() &&
-                  // eiter tsan or omp function
-                  // omp function necessary e.g. to keep synchronization
-                  (call->getCalledFunction()->getName().starts_with("__tsan") ||
-                   is_thread_function(call->getCalledFunction()))) {
-                if (call->getCalledFunction()->getName() ==
-                    "__tsan_func_exit") {
-                  if (dyn_cast<ResumeInst>(
-                          call->getNextNonDebugInstruction())) {
-                    // TODO make this a compiler option!!
-                    //  in cleanup block, dont handle exceptions
-                    continue;
-                  }
-                  if (is_tsan_cleanup_block(call->getParent())) {
-                    // skip, the tsan cleanup part.
-                    // no need to precompute, as it will only handle fatal
-                    // exceptions
-                    continue;
-                  }
-                }
-                if (is_func_from_std(call->getFunction())) {
-                  // dont analyze internals of std, though tsan may instrument
-                  // them
-                  continue;
-                }
-
-                for (auto it_arg = call->arg_begin(); it_arg != call->arg_end();
-                     ++it_arg) {
-                  to_precompute.push_back(it_arg->get());
-                }
-                precompute_locations.push_back(call);
-              }
-            }
-          }
-        }
-      }
-    }
-
     errs() << "\n";
-    errs() << "Statistics: locations: " << precompute_locations.size()
-           << " values: " << to_precompute.size() << "\n\n";
-
-    // no tsan found
-    if (precompute_locations.empty()) {
-      // no modification
-      return PreservedAnalyses::all();
-    }
-
-    auto precalcuation = std::make_shared<PrecomputeInsertion>(
-        M,
-        std::make_shared<PrecalculationAnalysis>(M, main_func, to_precompute,
-                                                 precompute_locations),
-        false);
-
-    // do NOT call clean_precompute() as we want the tsan calls to stick around
-
-    // we don't need the management stuff, we directly replace our program with
-    // precomputed one
-    auto precompute_main = precalcuation->get_precompute_main();
-    auto it = precompute_main->begin()->begin();
-    ++it; // second instruction is call to precomputed main
-    auto *call = cast<CallBase>(it);
-    auto precomputed_main = call->getCalledFunction();
-
-    // remove old main
-    auto orig_linkeage = main_func->getLinkage();
-    main_func->deleteBody(); // will set linkeage to weak, which we dont want
-    main_func->setLinkage(orig_linkeage);
-
-    // create new function body
-    auto *bb = BasicBlock::Create(M.getContext(), "entry", main_func);
-    // add call to precompute
-    IRBuilder<> builder = IRBuilder<>(bb);
-    // forward args of main
-    std::vector<Value *> args;
-    for (auto &arg : main_func->args()) {
-      args.push_back(&arg);
-    }
-
-    builder.CreateCall(precomputed_main, args);
-    builder.CreateRet(Constant::getNullValue(main_func->getReturnType()));
-
-    remove_noinline_from_module(M);
-
-    // remove other non-precompute functions now
-    std::vector<Function *> to_delete;
-    for (auto it_f = M.begin(); it_f != M.end(); ++it_f) {
-      Function *f = &*it_f;
-      if (precalcuation->is_func_part_of_precompute_phase(f)) {
-        // the tsan calls are already part of precompute, no need to instrumente
-        // them again
-        f->removeFnAttr(Attribute::SanitizeThread);
-      } else if ((not f->isDeclaration()) && f != main_func &&
-                 (not f->getName().starts_with("__tsan")) &&
-                 (not is_func_from_std(f))) {
-        // not used: remove
-        if (f->hasExternalLinkage())
-          f->setLinkage(GlobalValue::InternalLinkage);
-        // this will prompt GlobalDCE to remove
-      }
-    }
-
-#ifdef VERBOSE_DEBUG_PRINTING
-    Debug(errs() << "After Modification:\n"; M.dump();
-          errs() << "END MODULE\n";);
-#endif
+    run_optimization_passes(M, AM, run_tsan, false);
 
 #ifndef NDEBUG
-    auto has_error = verifyModule(M, &errs(), nullptr);
-    assert(!has_error);
+    auto num_undef = get_num_undefs(M);
+#endif
+    if (not run_optimization_passes(M, AM, run_precompute))
+      return PreservedAnalyses::all();
+#ifndef NDEBUG
     // at most: every undef value can be duplicated
     // TODO re-enable assertions for no openmp programs
     // assert(get_num_undefs(M) <= num_undef * 2);
@@ -281,27 +289,11 @@ struct SanitizerPrecomputePass : public PassInfoMixin<SanitizerPrecomputePass> {
     // elems are undef)
 #endif
 
-    errs() << "Successfully computed the precomputation\n\n";
-
-    run_optimization_passes(M, AM);
-
-    delete analysis_results;
-
-    return PreservedAnalyses::none();
-
-    errs() << "Optimize Loops\n\n";
-    Optimize_loops(M);
-
-#ifndef NDEBUG
-    has_error = verifyModule(M, &errs(), nullptr);
-    assert(!has_error);
+#ifdef PRECOMPUTE_TSAN_OPTIMIZE_LOOPS
+    run_optimization_passes(M, AM, Optimize_loops);
 #endif
 
-    /* Debug(errs() << "After Modification:\n"; M.dump();
-           errs() << "END MODULE\n";);*/
-
     delete analysis_results;
-
     return PreservedAnalyses::none();
   }
 };
