89bf1664e91088b48964777bf50e7a6c6e593809
Jan Braun <jan.braun@stud.tu-darmstadt.de>
2025-11-05 16:06:52 +0100

    replace compiler wrapper with python script and fix fortran testing

---
+++ clang_wrapper.py
@@ -0,0 +1,181 @@
+#!/usr/bin/env python3
+import os
+import sys
+import subprocess
+import tempfile
+import shutil
+
+program_name = os.path.basename(sys.argv[0])
+if program_name in ("clang_wrap_cc", "clang_wrapper", "clang"):
+    compiler = "clang"
+elif program_name in ("clang_wrap_cxx", "clang++_wrapper", "clang++"):
+    compiler = "clang++"
+elif program_name in ("flang_wrap", "flang_wrapper", "flang"):
+    compiler = "flang"
+elif program_name in ("flang_wrap_new", "flang-new_wrapper", "flang-new"):
+    compiler = "flang-new"
+else:
+    print(
+        f"Error: unknown wrapper invocation '{program_name}' - expected clang_wrap_cc, clang_wrap_cxx, or flang_wrap"
+    )
+    sys.exit(1)
+
+if "flang" in compiler:
+    compiler_type = "fortran"
+elif "clang" in compiler:
+    compiler_type = "cpp"
+else:
+    print(f'unknown compiler used: "{compiler}"')
+    sys.exit(1)
+
+ld_preload_prev = os.environ.get("LD_PRELOAD", "")
+debug_wrapper = os.environ.get("DEBUG_CLANG_WRAPPER", "false").lower() == "true"
+use_compiler_pass_env = os.environ.get("USE_COMPILER_PASS", "0")
+use_compiler_pass = (
+    use_compiler_pass_env == "1" or use_compiler_pass_env.lower() == "true"
+)
+
+if debug_wrapper:
+    print("INVOKE CLANG_WRAPPER")
+    print(program_name, " ".join(sys.argv[1:]))
+    try:
+        libasan = subprocess.check_output(
+            ["clang", "-print-file-name=libclang_rt.asan.so"], text=True
+        ).strip()
+        os.environ["LD_PRELOAD"] = f"{libasan}:{ld_preload_prev}"
+    except subprocess.CalledProcessError:
+        pass
+
+args = sys.argv[1:]
+is_to_obj = False
+has_o_option = False
+has_o_files = False
+has_flto = False
+has_fwhole_program_vtables = False
+has_opt_lvl = False
+has_src_file = False
+has_multiple_src_file = False
+
+for arg in args:
+    if arg == "-c":
+        is_to_obj = True
+    elif arg == "-o":
+        has_o_option = True
+    elif arg.endswith(".o"):
+        has_o_files = True
+    elif arg == "-flto":
+        has_flto = True
+    elif arg == "-fwhole-program-vtables":
+        has_fwhole_program_vtables = True
+    elif arg in ("-O1", "-O2", "-O3"):
+        has_opt_lvl = True
+    elif any(arg.endswith(ext) for ext in [".c", ".cpp", ".cc", ".cxx"]):
+        if has_src_file:
+            has_multiple_src_file = True
+        has_src_file = True
+
+# check if necessary flags are given
+if use_compiler_pass and (
+    not has_flto
+    or (not has_fwhole_program_vtables and compiler_type != "fortran")
+    or not has_opt_lvl
+):
+    print(
+        "Error, need -flto and -fwhole-program-vtables and at least -O1 for pass to work correctly"
+    )
+    sys.exit(1)
+
+if use_compiler_pass and "COMPILER_PASS" not in os.environ:
+    print("The COMPILER_PASS environment variable is not set")
+    sys.exit(1)
+
+
+def run_command(cmd):
+    if debug_wrapper:
+        print("RUN:", " ".join(cmd))
+    result = subprocess.call(cmd)
+    sys.exit(result)
+
+
+if is_to_obj:
+    if debug_wrapper:
+        print("MODE: to obj file")
+    if has_multiple_src_file:
+        print(
+            "ERROR linking multiple src files directly into one object file is not supported"
+        )
+        print("Compile one by one and link afterwards")
+        sys.exit(1)
+
+    cmd = [compiler]
+    for arg in args:
+        if arg == "-c":
+            cmd += ["-c", "-emit-llvm"]
+        elif arg.endswith(".o"):
+            # Remove the ".o" suffix and append ".bc"
+            new_file = arg[:-2] + ".bc"
+            cmd.append(new_file)
+            # mimic "touch"
+            open(arg, "a").close()
+            os.utime(arg, None)
+        else:
+            cmd.append(arg)
+
+    run_command(cmd)
+
+if has_o_files:
+    if debug_wrapper:
+        print("MODE: Link .o files")
+
+    # -x ir - : read ir from stdin
+    tmp_file = tempfile.NamedTemporaryFile(delete=False, suffix=".bc").name
+    cmd = [compiler, tmp_file]
+    if use_compiler_pass:
+        cmd += ["-fpass-plugin=" + os.environ["COMPILER_PASS"], "-lprecompute"]
+
+    llvm_link = ["llvm-link", "-o", tmp_file]
+
+    for arg in args:
+        if arg.endswith(".o"):
+            # Remove the ".o" suffix and append ".bc"
+            new_file = arg[:-2] + ".bc"
+            # remove from compiler invocation and add to file list
+            llvm_link.append(new_file)
+        elif arg.endswith(".so"):
+            # in our mode we cannot enter .o and .so files so we need to tell it to link it with -l
+            base = os.path.basename(arg)
+            # Use parameter expansion to remove file extensions
+            lib_fname = os.path.splitext(base)[0]
+            # Use parameter expansion to remove "lib" from the beginning
+            lib_name = lib_fname[3:] if lib_fname.startswith("lib") else lib_fname
+            # Use dirname to get the directory part (will at least result in ".")
+            directory = os.path.dirname(arg) or "."
+            cmd += [f"-L{directory}", f"-l{lib_name}"]
+        else:
+            cmd.append(arg)
+
+    if debug_wrapper:
+        print(f"{llvm_link} && {cmd}")
+
+    try:
+        subprocess.check_call(llvm_link)
+        run_command(cmd)
+    except subprocess.CalledProcessError:
+        pass
+
+if debug_wrapper:
+    print("MODE: direct to Binary")
+
+if has_multiple_src_file:
+    print(
+        "ERROR linking multiple src files directly into one binary file is not supported"
+    )
+    print("Compile one by one and link afterwards")
+    sys.exit(1)
+
+cmd = [compiler]
+if use_compiler_pass:
+    cmd += ["-fpass-plugin=" + os.environ["COMPILER_PASS"], "-lprecompute"]
+cmd += args
+
+run_command(cmd)
