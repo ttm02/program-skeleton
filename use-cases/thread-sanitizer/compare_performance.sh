607802ee779372edd6ba9efc31649c982f8a8ebc
Jan Braun <jan.braun@stud.tu-darmstadt.de>
2025-11-24 13:40:14 +0100

    rework performance/timing script

--- use-cases/thread-sanitizer/compare_performance.sh
+++ use-cases/thread-sanitizer/compare_performance.sh
@@ -1,85 +1,84 @@
 #!/usr/bin/env bash
 
-echo $(pwd)
-# where the wrappers are found
-BINARY_DIR=$1
+usage() {
+  echo "$0 <build dir>"
+  exit 1
+}
 
-# the directory with the drb testcases
-TEST_CASE_DIR=$2
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+PRECOMPUTE_DIR=$(git -C "${SCRIPT_DIR}" rev-parse --show-toplevel)
 
+MY_CMAKE_BUILD_DIR="${PRECOMPUTE_DIR}/build"
 
-TEST_CASES=$(ls $TEST_CASE_DIR)
+if [ -n "$1" ]; then
+  if ! [ -d "$1" ]; then
+    usage
+  else
+    MY_CMAKE_BUILD_DIR="$1"
+  fi
+fi
 
+BINARY_DIR="${MY_CMAKE_BUILD_DIR}/use-cases/thread-sanitizer"
 
-source ${BINARY_DIR}/setup_env.sh
-RUN_SCRIPT=$BINARY_DIR/run.sh
+# TODO fortran testcases
+TEST_CASE_DIR="${MY_CMAKE_BUILD_DIR}/_deps/drb-src/micro-benchmarks"
+TEST_CASES=$(ls "$TEST_CASE_DIR")
 
-#compares standard thread sanitizer with the precomputed one
+source "${BINARY_DIR}/setup_env.sh"
+RUN_SCRIPT="${BINARY_DIR}/run.sh"
 
 GREP_STRING="WARNING: ThreadSanitizer: data race"
 
-# the result file
-echo "time_original,time_precompute" > timing.csv
+echo "testcase,time_original,time_precompute,found_by" >timing.csv
 
-for TEST_CASE in $TEST_CASES
-do
-  if [[ $TEST_CASE == *.c ]]; then
+save_time_to_file() {
+  (
+    echo -n "$1"
+    echo -n ","
+    cat "${MY_TMP_DIR}/time_orig.log" | tr -d "\n"
+    echo -n ","
+    cat "${MY_TMP_DIR}/time_precompute.log" | tr -d "\n"
+    echo -n ","
+    echo "$2"
+  ) | tee -a timing.csv
+}
 
+MY_TMP_DIR=$(mktemp -d --suffix="JustTheRaces-timing")
 
-rm ./a.out ./a.out_original
+for TEST_CASE in $TEST_CASES; do
+  if [[ "$TEST_CASE" == *.c ]]; then
+    rm -f ./a.out ./a.out_original
 
-if grep -q 'PolyBench' "$TEST_CASE_DIR/$TEST_CASE"; then
-  echo "Skip testcase for now, it needs different compile flags"
-#  additional_compile_flags+=" $POLYFLAG";
-continue
-fi
+    # compile
+    $RUN_SCRIPT "${TEST_CASE_DIR}/${TEST_CASE}" &>/dev/null
 
+    if [[ -x "./a.out" ]]; then
+      # compilation successful
+      /usr/bin/env time -f "%e" -o "${MY_TMP_DIR}/time_orig.log" \
+        --quiet ./a.out_original &>"${MY_TMP_DIR}/orig.log"
+      /usr/bin/env time -f "%e" -o "${MY_TMP_DIR}/time_precompute.log" \
+        --quiet ./a.out &>"${MY_TMP_DIR}/precompute.log"
 
-
-POLYFLAG="micro-benchmarks/utilities/polybench.c -I micro-benchmarks -I micro-benchmarks/utilities -DPOLYBENCH_NO_FLUSH_CACHE -DPOLYBENCH_TIME -D_POSIX_C_SOURCE=200112L"
-
-
-echo "case $TEST_CASE"
-
-# compile
-$RUN_SCRIPT $TEST_CASE_DIR/$TEST_CASE &>/dev/null
-
-if [[ -x "./a.out" ]]; then
-    # a.out exists
-/usr/bin/time -f "%e" -o time_orig.log --quiet ./a.out_original &> orig.log
-/usr/bin/time -f "%e" -o time_precompute.log --quiet ./a.out &> precompute.log
-if grep -qF "$GREP_STRING" orig.log; then
-
-        if grep -qF "$GREP_STRING" precompute.log; then
-          # success
-          time_original=$(cat time_orig.log)
-          time_precompute=$(cat time_precompute.log)
-          echo "both versions found the datarace"
-          echo "$time_original,$time_precompute" >> timing.csv
-
+      if grep -qF "$GREP_STRING" "${MY_TMP_DIR}/orig.log"; then
+        if grep -qF "$GREP_STRING" "${MY_TMP_DIR}/precompute.log"; then
+          save_time_to_file "$TEST_CASE" "both"
         else
-            echo "Original sanitizer found the data race but precomputed not"
-
+          save_time_to_file "$TEST_CASE" "orig"
         fi
+      else
+        if grep -qF "$GREP_STRING" "${MY_TMP_DIR}/precompute.log"; then
+          save_time_to_file "$TEST_CASE" "precomputed"
+        else
+          save_time_to_file "$TEST_CASE" "neither"
+        fi
+      fi
     else
-        #echo "Original sanitizer found no race"
-        if grep -qF "$GREP_STRING" precompute.log; then
-                  echo "Original sanitizer found no race but precomputed did"
-                else
-                  #success
-                    time_original=$(cat time_orig.log)
-                    time_precompute=$(cat time_precompute.log)
-                    echo "both versions found no race"
-                    echo "$time_original,$time_precompute" >> timing.csv
-        fi
+      echo "0" >"${MY_TMP_DIR}/time_orig.log"
+      echo "0" >"${MY_TMP_DIR}/time_precompute.log"
+      save_time_to_file "$TEST_CASE" "compilation failed"
     fi
-else
-    echo "Compilation fail"
-fi
-
-fi
-
+  fi
 done
 
-
-
+rm -fr "$MY_TMP_DIR"
+exit 0
