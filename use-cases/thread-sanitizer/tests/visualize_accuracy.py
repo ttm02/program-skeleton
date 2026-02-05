0f58f002ef8b5121d8cda52e3b556949f27f27ea
Jan Braun <jan.braun@stud.tu-darmstadt.de>
2026-02-05 11:53:24 +0100

    add heatmap to accuracy visualisation

--- use-cases/thread-sanitizer/tests/visualize_accuracy.py
+++ use-cases/thread-sanitizer/tests/visualize_accuracy.py
@@ -113,13 +113,65 @@ def create_plot(df, pdf, pdf_name):
     plt.close()
 
 
+def create_heat(df, pdf, pdf_name):
+    df_tc_count = df["testcase"].nunique()
+    fig_factor = max(1 - df_tc_count * 0.05, 0)
+    fig_height = df_tc_count * (0.32 + fig_factor * 0.06) + (0.8 + fig_factor * 0.3)
+    fig, ax = plt.subplots(figsize=(8.4, fig_height))
+
+    bins = [0, 5, 10, 20, 40, 60, 80, 90, 95, 100]
+    labels = [
+        "0–5",
+        "5-10",
+        "10-20",
+        "20-40",
+        "40-60",
+        "60-80",
+        "80-90",
+        "90-95",
+        "95-100",
+    ]
+
+    df = df.copy()
+    df["value_bin"] = pd.cut(
+        df["df_value_count"], bins=bins, labels=labels, include_lowest=True
+    )
+    freq = pd.crosstab(df["testcase"], df["value_bin"])
+    percent = freq.div(freq.sum(axis=1), axis=0) * 100
+
+    # ax.yaxis.set_label_position("right")
+    # ax.yaxis.tick_right()
+
+    sns.heatmap(
+        percent,
+        annot=True,
+        fmt=".0f",
+        cmap="YlOrRd",
+        cbar_kws={"label": "Percentage (%)"},
+    )
+
+    ax.set_title(f"DataRaceBench ({pdf_name}): Accuracy")
+    ax.set_xlabel("Detection Percentage (in %)")
+    ax.set_ylabel("Testcase\nNames" if fig_height < 2.63 else "Testcase Names")
+
+    plt.tight_layout()
+    pdf.savefig(fig)
+    plt.close()
+
+
 def get_plot(df, pdf_name):
     with PdfPages(f"DRB_Accuracy_{pdf_name}.pdf") as pdf:
         # create dummy page as first page as it often renders not so nice
         create_plot(df[df["testcase"] == df["testcase"].iloc[0]], pdf, "to be ignored")
 
         for df_chunk in split_dataframe(df, 16):
-            create_plot(df_chunk, pdf, pdf_name)
+            # create_plot(df_chunk, pdf, pdf_name)
+            for mo in mode_order:
+                create_heat(
+                    df_chunk[df_chunk["mode_readable"] == mo],
+                    pdf,
+                    pdf_name + " - " + mo,
+                )
 
     print(f"Saving DRB_Accuracy_{pdf_name}.pdf")
 
@@ -150,11 +202,11 @@ def visualize_accuracy():
 
         return df_cat
 
-    df_yes = get_df_tc_cat(df, "yes")
     df_no = get_df_tc_cat(df, "no")
-
-    get_plot(df_yes, "yes")
     get_plot(df_no, "no")
+
+    df_yes = get_df_tc_cat(df, "yes")
+    get_plot(df_yes, "yes")
 
 
 if __name__ == "__main__":
