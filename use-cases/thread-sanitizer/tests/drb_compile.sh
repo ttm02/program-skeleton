696ef4eddf38526b7fb041351d95cd24e8b181ca
Jan Braun <jan.braun@stud.tu-darmstadt.de>
2026-02-28 00:17:40 +0100

    make sure static analysis is of the passthrough

--- use-cases/thread-sanitizer/tests/drb_compile.sh
+++ use-cases/thread-sanitizer/tests/drb_compile.sh
@@ -51,12 +51,10 @@ if [ -z "$OUTPUT_SUFFIX" ]; then
     fi
 fi
 
-if [ "$USE_STATIC_ANALYSIS" = 'true' ]; then
-    CFLAGS="$CFLAGS --enable-static-analysis"
-fi
-
 if [ -n "$MY_STAN_PASS_MODE" ]; then
     CFLAGS="$CFLAGS $MY_STAN_PASS_MODE_ARGS"
+elif [ "$USE_STATIC_ANALYSIS" = 'true' ]; then
+    CFLAGS="$CFLAGS --enable-static-analysis"
 fi
 
 rm -f ./a.out_"${OUTPUT_SUFFIX}"
